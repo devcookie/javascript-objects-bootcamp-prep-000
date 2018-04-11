004aefa77c23185858c8615a44eedc65b6556555
Catherine Lee <lee.catherine77@gmail.com>
2018-04-11 23:07:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {
   'REM': 'Orange Crush'
-}
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign(playlist, {[artistName]: songTitle})
+  return Object.assign(playlist, {[artistName]: songTitle});
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist.artistName;
 }
