09671a4def090d3a928f3dabece254773fcbc16a
Catherine Lee <lee.catherine77@gmail.com>
2018-04-11 23:16:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+  // delete playlist.artistName didn't work...why?
   delete playlist[artistName];
   return playlist;
 }
